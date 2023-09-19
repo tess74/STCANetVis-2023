7dfe597f837dc6abea8fe5b368946678a52a282c
shabs76 <shabsmwanga76@gmail.com>
2023-09-19 10:21:39 +0300

    final commit for deployment

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : '';
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://scanet.api.brentles.co.tz';
 
 export const socket = io(URL);
