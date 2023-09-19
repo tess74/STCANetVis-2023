13f1212485f1881cc48b078706eb97cf1334941f
shabs76 <shabsmwanga76@gmail.com>
2023-09-19 10:54:27 +0300

    fixing production url for socket io

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://scanet.api.brentles.co.tz';
+const URL = process.env.NODE_ENV === 'production' ? 'https://scanet.api.brentles.co.tz' : 'https://scanet.api.brentles.co.tz';
 
 export const socket = io(URL);
